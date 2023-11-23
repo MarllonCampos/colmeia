36ebe4d1695030aa8e63eee21c10d70d97152b72
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-11-23 18:21:48 -0300

    feat: create a file for fizzBuzz and delete index.ts

--- fizzBuzz.ts
+++ fizzBuzz.ts
@@ -1,4 +1,13 @@
-function fizzBuzz(numbers: Array<number>) {
+export function fizzBuzz(numbers: Array<number>) {
+  // Existem diversas formas de realizar esse método porém decidi seguir pelo forEach
+  let finalResponse = '';
+  numbers.forEach((number) => {
+    let response = '';
+    if (number % 3 == 0) response += 'fizz';
+    if (number % 5 == 0) response += response.length > 0 ? 'Buzz' : 'buzz';
+    finalResponse += `${number} - ${response}\n`;
+  });
+
   // for (let number of numbers) {
   //   let response = '';
   //   if (number % 3 == 0) response += 'fizz';
@@ -6,20 +15,15 @@ function fizzBuzz(numbers: Array<number>) {
   //   console.log(`${number} - ${response}`);
   // }
 
-  // Existem diversas formas de realizar esse método porém decidi seguir pelo forEach
-  numbers.forEach((number) => {
-    let response = '';
-    if (number % 3 == 0) response += 'fizz';
-    if (number % 5 == 0) response += response.length > 0 ? 'Buzz' : 'buzz';
-    console.log(`${number} - ${response}`);
-  });
-
   // for (let i = 0; i < numbers.length; i++) {
   //   let response = '';
   //   if (numbers[i] % 3 == 0) response += 'fizz';
   //   if (numbers[i] % 5 == 0) response += response.length > 0 ? 'Buzz' : 'buzz';
   //   console.log(`${numbers[i]} - ${response}`);
   // }
+
+  console.log(finalResponse);
+  return finalResponse;
 }
 
 const numbersArray = [3, 5, 9, 15, 29, 27, 32, 45];
